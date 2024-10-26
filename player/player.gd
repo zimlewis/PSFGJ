0f6d12dc46670070727675f6d31fdec696e33b09
Zimlewis <z1ml3w1s123@gmail.com>
2024-10-26 18:02:32 +0700

    coyote_time added

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 40
 const DECCELERATION: = 60
 
+@export var sprite: AnimatedSprite2D
+var coyote_time: float = 1
+
+
+func _process(delta: float) -> void:
+	coyote_time -= delta
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,11 +18,21 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		coyote_time = 0
+		velocity.y = 0
 	
+	if Input.is_action_pressed("ui_accept") and coyote_time == 0:
+		velocity.y = JUMP_VELOCITY
+		sprite.play("jump")
+	
+	# Play fall animation
+	if velocity.y > 0:
+		sprite.play("falling")
+	
+	# Cut out the jump if release the button early
 	if !is_on_floor() and Input.is_action_just_released("ui_accept") and velocity.y < 0:
-		velocity.y *= 0.5           
+		velocity.y *= 0.5
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
